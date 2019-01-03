c65e7b7b947ae1c5a30074c1b77392ff2bd68f8a
lmg <qwer1234@163.com>
2019-01-03 19:52:08 +0800

    print i

--- git123/testgithub/src/com/itheima/test.java
+++ git123/testgithub/src/com/itheima/test.java
@@ -8,5 +8,13 @@ public class test {
         System.out.println("hhhhhhhhh");
         //update1
         System.out.println("123321");
+
+
+    }
+    public void test() {
+        int i=0;
+        for (int i1 = 0; i1 < 10; i1++) {
+            System.out.println(i1);
+        }
     }
 }
